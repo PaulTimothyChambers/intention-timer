f2218e1feb7e7acef066dabac65e13d1971b9c0f
Paul Chambers <paultimothychambers@gmail.com>
2021-07-28 23:28:33 -0600

    Add more pseudocode

--- Activity.js
+++ Activity.js
@@ -8,14 +8,17 @@ class Activity {
     this.completed = []
   }
   // will initiate countdown timer (using numbers input by user) upon btn click "Start Activity", and alert user when time is up, and then invoke markComplete() func
+// counts down, then alerts user of time up, creates new instance of activity, and invokes next method
   countdown() {
-    
+
   }
   // will push dynamic HTML into completed activities array and changing the appropriate HTML element to match, then invoke saveToStorage()
+// assign vars dynamically to "keys" (instances of Activity class), pushes dynamic html with instance interpolated into this.completed array
   markComplete() {
 
   }
   // will save completed activities to local storage
+// uses docqueryselec.values and passes them to storage along with vars created by markComplete as the keys (which are passed using quotes: "key")
   saveToStorage() {
 
   }
