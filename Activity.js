71331d8b85a7bb01cc4e48a5318c54ab6b16d86f
Paul Chambers <paultimothychambers@gmail.com>
2021-07-28 18:45:07 -0600

    Build empty class method shells and peusocode their function

--- Activity.js
+++ Activity.js
@@ -5,14 +5,17 @@ class Activity {
     this.description = input;
     this.minutes = min;
     this.seconds = sec;
-    this.isCompleted = false
+    this.completed = []
   }
+  // will initiate countdown timer (using numbers input by user) upon btn click "Start Activity", and alert user when time is up, and then invoke markComplete() func
   countdown() {
     
   }
+  // will push dynamic HTML into completed activities array and changing the appropriate HTML element to match, then invoke saveToStorage()
   markComplete() {
 
   }
+  // will save completed activities to local storage
   saveToStorage() {
 
   }
