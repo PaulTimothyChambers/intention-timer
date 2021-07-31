f8ec4561672be0ba35926cf70594df5675c4de9b
Susanna Carey <susannaopal@gmail.com>
2021-07-30 20:56:38 -0400

    Add update to files to create new classes to change colorrs upon click event for activities

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ var countdownTimer = document.querySelector('.countdown');
 var startActivity = document.querySelector('.btn-start-activity');
 var inputMinute = document.querySelector('#min');
 var inputSecond = document.querySelector('#sec');
-
+//
+// var studyBtn = document.querySelector('#study-button');
+// var meditateBtn = document.querySelector('#meditate-button');
+// var exerciseBtn = document.querySelector('#exercise-button');
 // var classInstance = new Activity('exercise', 'Doing things', 5, 34);
+var btnActivities = document.querySelector('.btn-activities');
+var studyBtn = document.querySelector('#study-btn');
+var meditateBtn = document.query('#meditate-btn');
+var exerciseBtn = document.querySelector('#exercise-btn');
+//query selectors for button and images
 
 startActivity.addEventListener('click', startCount)
+btnActivities.addEventListener('click', clickActivityButton)
+studyBtn.addEventListener('click', changeStudyColor);
+meditateBtn.addEventListener('click', changeMeditateColor);
+exerciseBtn.addEventListener('click', changeExerciseColor);
 
 // function startCount() {
 //   countdownTimer.classList.remove('hidden');
@@ -14,7 +26,7 @@ startActivity.addEventListener('click', startCount)
 //   var seconds = inputSecond.value;
 //   var minutes = inputMinute.value * 60;
 //   let time = parseInt(minutes) + parseInt(seconds)
-// 
+//
 //     function countdownDisplay() {
 //       const minutesLeft = Math.floor(time / 60);
 //       let secondsLeft = time % 60;
@@ -22,4 +34,26 @@ startActivity.addEventListener('click', startCount)
 //       document.querySelector('.countdown').innerHTML = `<span>${minutesLeft}: ${secondsLeft}</span>`
 //       time--;
 //     }
+}
+//changing colors on the buttons when they are clicked
+//need to take click eventListener to instigate the function
+//if clicled -- check and change
+
+// function changeColor() {
+//
+// }
+function changeStudyColor() {
+studyBtn.classList.add('study-text-top-button');
+studyBtn.classList.add('study-btn-activities');
+studyBtn.classList.add('study-image-activities');
+}
+
+function changeMeditateColor(){
+meditateBtn.classList.add('meditate-text-top-button');
+meditateBtn.classList.add('meditate');
+meditateBtn.classList.add('');
+}
+
+function changeExerciseColor(){
+
 }
