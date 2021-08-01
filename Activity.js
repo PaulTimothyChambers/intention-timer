ab7e8289bcc3327d5fc1f0ad8aa843f106c5d637
Paul Chambers <paultimothychambers@gmail.com>
2021-08-01 15:52:38 -0600

    Create functional timer

--- Activity.js
+++ Activity.js
@@ -5,15 +5,39 @@ class Activity {
     this.description = input;
     this.minutes = min;
     this.seconds = sec;
-    this.completed = []
+    this.completed = [];
   }
   // will initiate countdown timer (using numbers input by user) upon btn click "Start Activity", and alert user when time is up, and then invoke markComplete() func
 // counts down, then alerts user of time up, creates new instance of activity, and invokes next method
   countdown() {
+    setInterval(countdownDisplay, 1000);
+    var seconds = this.seconds;
+    var minutes = this.minutes * 60;
+    let time = parseInt(minutes) + parseInt(seconds);
 
+      function countdownDisplay() {
+        if (time >= 0) {
+          var minutesLeft = Math.floor(time / 60);
+          var secondsLeft = time % 60;
+          secondsLeft = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+          document.querySelector('.countdown').innerHTML = `
+            <div class="base-timer">
+              <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g class="base-timer__circle">
+                  <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                </g>
+              </svg>
+              <span id="base-timer-label" class="base-timer__label">
+                ${minutesLeft}: ${secondsLeft}
+              </span>
+            </div>`
+          time--;
+        } else {
+          return;
+        }
+      }
   }
-  // will push dynamic HTML into completed activities array and changing the appropriate HTML element to match, then invoke saveToStorage()
-// assign vars dynamically to "keys" (instances of Activity class), pushes dynamic html with instance interpolated into this.completed array
+
   // markComplete(newInstance) {
   //   completedActivities.innerHTML +=
   //     <section class="">
@@ -23,8 +47,7 @@ class Activity {
   //       etc.
   //     </section>
 // }
-  // will save completed activities to local storage
-// uses docqueryselec.values and passes them to storage along with vars created by markComplete as the keys (which are passed using quotes: "key")
+
   saveToStorage(newInstance) {
     localStorage.setItem('completedActivity', JSON.stringify(newInstance))
   }
